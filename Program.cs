2bcaff51cb128013141815cc79c12da5073ddad9
ed.lopez1905@gmail.com <ed.lopez1905@gmail.com>
2018-08-16 18:47:11 -0700

    upload

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BooleanLogicproject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int whatAge;
+            bool anyDui;
+            int speedingTickets;
+
+
+            Console.WriteLine("What is your age? ");
+            whatAge = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Have you ever had a DUI? True or False");
+            anyDui = Convert.ToBoolean(Console.ReadLine());
+
+
+            Console.WriteLine("How many speeding tickets have you had? If none, please type in 0");
+            speedingTickets = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Qualified?");
+
+            if (whatAge >= 15 && anyDui == false && speedingTickets <= 2)
+            {
+
+                Console.WriteLine("True");
+            }else
+            {
+                Console.WriteLine("False");
+            }
+
+            
+
+            Console.ReadLine();
+
+        }
+    }
+}
